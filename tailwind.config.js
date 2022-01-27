6055d62374697a6802fcc0fc0a43ac28e08e22ce
LeticiaGenadeze <lety_genadeze@hotmail.com>
2022-01-27 09:18:24 -0300

    Start projeto

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        '30':'7.5rem',
+        '290px':'290px',
+        '350px':'350px',
+        '396px':'396px',
+        '456px':'446px',
+        '500px':'500px',
+        '980px':'980px',
+        '1100px':'1100px'
+      },
+      fontSize:{
+        'subtitle':'1.7rem'
+      },
+      colors:{
+        'main-gray':'#F0F2F5',
+        'main-blue':'#1877F2',
+        'main-blue-hover':'#166FE5',
+        'main-green':'#42B72A',
+        'main-green-hover':'#36A420',
+        'border-gray':'#ecedef', 
+        'text-gray':'#c4c6c8'
+      }
+    },
+  },
+  plugins: [],
+}
